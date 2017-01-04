9841da9dc9ae161961c1f63ebf0d966e043b233d
lurquhar1221 <minecraft2314@gmail.com>
2017-01-04 17:18:34 +1300

    Make GUI sorta-exist

--- src/main/java/club/krist/lc/lunarcomputing/gui/GUIComputer.java
+++ src/main/java/club/krist/lc/lunarcomputing/gui/GUIComputer.java
@@ -2,6 +2,7 @@ package club.krist.lc.lunarcomputing.gui;
 
 import club.krist.lc.lunarcomputing.tile.TileComputer;
 import net.minecraft.client.gui.GuiScreen;
+
 public class GUIComputer extends GuiScreen {
 
     public GUIComputer(TileComputer computer) {
@@ -11,5 +12,11 @@ public class GUIComputer extends GuiScreen {
     @Override
     public boolean doesGuiPauseGame() {
         return false;
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
     }
 }
